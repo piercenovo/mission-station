11c51305a26bf8152b7eb033912acc44c6dc5dd2
piercenovo <piercenovoah@gmail.com>
2025-02-24 23:38:46 -0500

    styles: customizing components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
-  return <div className="text-red-600">Hello test component</div>;
+  return (
+    <div className="">
+      <Input />
+      <Button>Primary</Button>
+      <Button variant="secondary">secondary</Button>
+      <Button variant="destructive">destructive</Button>
+      <Button variant="ghost">ghost</Button>
+      <Button variant="muted">muted</Button>
+      <Button variant="outline">outline</Button>
+      <Button variant="teritaty">teritaty</Button>
+    </div>
+  );
 }
